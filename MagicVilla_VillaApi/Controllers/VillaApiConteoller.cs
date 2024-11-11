6aa9e70408da0d424dd0421936eb828108f0235a
Ahmed Mohsen <163747371+amohsen98@users.noreply.github.com>
2024-11-11 18:21:49 +0200

    First Model

---
+++ MagicVilla_VillaApi/Controllers/VillaApiConteoller.cs
@@ -0,0 +1,28 @@
+﻿using MagicVilla_VillaApi.Models;
+using MagicVilla_VillaApi.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaApi.Controllers
+{
+    //[Route("api/[controller]")]
+
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiConteoller : ControllerBase
+
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            //Return Json File key value pair 
+            return new List<VillaDTO>()
+            {
+
+                new VillaDTO { Id = 1, Name = "Pool View" },
+                new VillaDTO { Id = 2, Name = "Beach View" }
+            };
+
+        }
+
+    }
+}
