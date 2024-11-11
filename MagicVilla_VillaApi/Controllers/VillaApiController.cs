fc482688f690690d1533c17a16810c5190701ddd
Ahmed Mohsen <163747371+amohsen98@users.noreply.github.com>
2024-11-11 18:50:14 +0200

    GET

--- MagicVilla_VillaApi/Controllers/VillaApiController.cs
+++ MagicVilla_VillaApi/Controllers/VillaApiController.cs
@@ -2,6 +2,7 @@
 using MagicVilla_VillaApi.Models;
 using MagicVilla_VillaApi.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace MagicVilla_VillaApi.Controllers
 {
@@ -9,7 +10,7 @@ namespace MagicVilla_VillaApi.Controllers
 
     [Route("api/VillaApi")]
     [ApiController]
-    public class VillaApiConteoller : ControllerBase
+    public class VillaApiController : ControllerBase
 
     {
         [HttpGet]
@@ -17,6 +18,14 @@ namespace MagicVilla_VillaApi.Controllers
         {
             //Return Json File key value pair 
             return VillaStore.VillaList;
+
+        }
+
+        [HttpGet("{id:int}")]
+        public VillaDTO GetVilla(int id)
+        {
+            //linq operation, deal with any kind of database, here is list
+            return VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
 
         }
 
--- MagicVilla_VillaApi/Data/VillaStore.cs
+++ MagicVilla_VillaApi/Data/VillaStore.cs
@@ -5,6 +5,7 @@ namespace MagicVilla_VillaApi.Data
     public static class VillaStore
     {
         //Object Initializer 
+        //static field doesn't have to be inisitiaited through an object, just the class name
         public static List<VillaDTO> VillaList = new List<VillaDTO>()
         
             {
